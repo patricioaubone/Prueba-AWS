288db07060f8402008204098adda50a56d86162f
patricioaubone <131826145+patricioaubone@users.noreply.github.com>
2023-04-27 19:15:08 -0300

    Update dag-s3.py

--- dag-s3.py
+++ dag-s3.py
@@ -11,5 +11,17 @@ df_advertiser_ids = pd.read_csv(obj['Body']) #levantamos el DF
 
 print(df_advertiser_ids.head())
 
+
+#Enviando a S3
+desde_EC2 = pd.DataFrame.to_csv(df_advertiser_ids)
+s3.put_object(Bucket=bucket_name, Key='desde_EC2.csv', Body=desde_EC2.encode('utf-8'))
+
+
+
+#Enviando a RDS
+
+
+
+
 # df_product_views = pd.read_csv(df_product_views)
 # df_ads_views = pd.read_csv(df_ads_views)
