b051ba3a5e58474d41a1f975900ef3777a9850f7
patricioaubone <131826145+patricioaubone@users.noreply.github.com>
2023-04-28 00:11:39 -0300

    Update dag-s3.py

--- dag-s3.py
+++ dag-s3.py
@@ -2,7 +2,7 @@ import boto3
 import pandas as pd
 
 s3 = boto3.client("s3") #definimos un cliente para trabajar con S3 usando boto3
-bucket_name = "data-raw-udesa-prueba1" #el nombre de nuestro bucket creado
+bucket_name = "bucket-udesa-prueba" #el nombre de nuestro bucket creado
 s3_object = "advertiser_ids.csv" #el archivo que vamos a traernos
 
 
