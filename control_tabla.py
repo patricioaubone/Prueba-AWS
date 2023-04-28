79c133ca37199633f6897f961bce79e400abe74f
patricioaubone <131826145+patricioaubone@users.noreply.github.com>
2023-04-28 19:12:59 -0300

    Update control_tabla.py

--- control_tabla.py
+++ control_tabla.py
@@ -15,17 +15,17 @@ conn = psycopg2.connect(host=host, database=database, user=user, password=passwo
 # Crear un cursor para ejecutar las consultas SQL
 cur = conn.cursor()
 
-cur.execute("SELECT * FROM information_schema.table_privileges WHERE grantee = 'postgres';")
-cur.execute("SELECT * FROM information_schema.role_table_grants WHERE table_name = 'tabla_RDS';")
+# cur.execute("SELECT * FROM information_schema.table_privileges WHERE grantee = 'postgres';")
+# cur.execute("SELECT * FROM information_schema.role_table_grants WHERE table_name = 'tabla_RDS';")
 
-# # Verificar si la tabla existe
-# cur.execute(f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}');")
-# exists = cur.fetchone()[0]
+# Verificar si la tabla existe
+cur.execute(f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}');")
+exists = cur.fetchone()[0]
 
-# if exists:
-#     print(f"La tabla {table_name} existe en la base de datos {database}.")
-# else:
-#     print(f"La tabla {table_name} no existe en la base de datos {database}.")
+if exists:
+    print(f"La tabla {table_name} existe en la base de datos {database}.")
+else:
+    print(f"La tabla {table_name} no existe en la base de datos {database}.")
 
 # Cerrar la conexión
 cur.close()
