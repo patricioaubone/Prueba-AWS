59255aec40a6cd26227e75817b2e480bd2e39c9c
patricioaubone <131826145+patricioaubone@users.noreply.github.com>
2023-04-27 23:54:30 -0300

    Update dag-s3.py

--- dag-s3.py
+++ dag-s3.py
@@ -2,7 +2,7 @@ import boto3
 import pandas as pd
 
 s3 = boto3.client("s3") #definimos un cliente para trabajar con S3 usando boto3
-bucket_name = "data-raw-udesa-prueba" #el nombre de nuestro bucket creado
+bucket_name = "data-raw-udesa-prueba1" #el nombre de nuestro bucket creado
 s3_object = "advertiser_ids.csv" #el archivo que vamos a traernos
 
 
@@ -29,30 +29,30 @@ s3.put_object(Bucket=bucket_name, Key='desde_EC2.csv', Body=desde_EC2.encode('ut
 
 
 #Enviando a RDS
-# import psycopg2
-# dbname = "database-1"
-# user = "postgres" #Configuracion / Disponibilidad / nombre de usuario maestro
-# password = "chavoLOCO23"
-# host = "database-1.c7mkdwca7kj0.us-east-1.rds.amazonaws.com" #Econectividad y seguridad
-# port = "5432"
+import psycopg2
+dbname = "basesql"
+user = "postgres" #Configuracion / Disponibilidad / nombre de usuario maestro
+password = "chavoLOCO23"
+host = "database-1.c7mkdwca7kj0.us-east-1.rds.amazonaws.com" #Econectividad y seguridad
+port = "5432"
 
-# #Creamos la conexión a RDS
-# conn = psycopg2.connect(
-#     dbname=dbname,
-#     user=user,
-#     password=password,
-#     host=host,
-#     port=port
-# )
+#Creamos la conexión a RDS
+conn = psycopg2.connect(
+    dbname=dbname,
+    user=user,
+    password=password,
+    host=host,
+    port=port
+)
 
 
 
-# # Insertar los registros en la tabla 'table_name'
-# table_name = 'table_name'
-# desde_EC2.to_sql(table_name, conn, if_exists='append', index=False)
+# Insertar los registros en la tabla 'table_name'
+table_name = 'tabla_RDS'
+desde_EC2.to_sql(table_name, conn, if_exists='append', index=False)
 
-# # Cerrar la conexión
-# conn.close()
+# Cerrar la conexión
+conn.close()
 
 
 
